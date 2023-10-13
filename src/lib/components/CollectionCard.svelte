b41316c327aa1002835c327dbd74ac9dc5696f1a
JorgeToAn <jorgetosta.f@gmail.com>
2023-10-13 00:06:58 -0700

    Collection card fix

--- src/lib/components/CollectionCard.svelte
+++ src/lib/components/CollectionCard.svelte
@@ -1,6 +1,6 @@
 <script>
     export let picture_path;
-    export let title;
+    export let name;
     export let href;
     export let artist;
 </script>
@@ -8,7 +8,7 @@
 <div class="collection-card transition ease-in rounded m-4 w-44 max-w-lg bg-primary-500 hover:bg-primary-800 hover:scale-110">
     <a {href}>
         <img src={picture_path} alt="Cover" class="rounded-t w-44 h-44 mx-auto">
-        <p class="title p-2 whitespace-nowrap text-ellipsis overflow-hidden">{title}</p>
+        <p class="title p-2 whitespace-nowrap text-ellipsis overflow-hidden">{name}</p>
         <p class="artist p-2 pt-0 whitespace-nowrap text-ellipsis overflow-hidden text-primary-100">{artist}</p>
     </a>
 </div>
